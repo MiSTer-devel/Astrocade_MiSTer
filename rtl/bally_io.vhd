56c93449f8c6b5d97bbc326e61b589673b2fbc31
Reggs01925 <jrostron@fastmail.co.uk>
2020-09-19 07:14:53 +0100

    Sound fixes to the Astrocade IO chip
    Fixes to bally_io.vhd - Fixes to Master Oscillator preset, Tremolo, Mux and Adder. Updated comments relate to Bally Astrocade Patent with component references in brackets - Reggs

--- rtl/bally_io.vhd
+++ rtl/bally_io.vhd
@@ -38,9 +38,11 @@
 --
 -- Revision list
 --
--- version 003 spartan3e release
+
 -- version 001 initial release
---
+-- version 003 spartan3e release
+-- version 004 (19/Sept/20) Sound Fixes to Master Oscillator preset, Tremolo, Mux and Adder - Reggs 
+
 library ieee;
   use ieee.std_logic_1164.all;
   use ieee.std_logic_arith.all;
@@ -99,12 +101,14 @@ architecture RTL of BALLY_IO is
   signal master_cnt           : std_logic_vector(7 downto 0);
   signal master_freq          : std_logic_vector(7 downto 0);
 
-  signal vibrato_cnt          : std_logic_vector(18 downto 0);
+  signal vibrato_cnt          : std_logic_vector(12 downto 0); -- 13 Bit counter (1556)
   signal vibrato_ena          : std_logic;
 
-  signal poly17               : std_logic_vector(16 downto 0);
-  signal noise_gen            : std_logic_vector(7 downto 0);
+  signal poly15               : std_logic_vector(14 downto 0):= "000000000000000"; --(1558 Noise Generator)
+  signal counter_up 		  : std_logic_vector(5 downto 0) := "000000";
+  signal counter	 		  : std_logic := '0';
 
+  signal noise_gen            : std_logic_vector(7 downto 0);
   signal tone_gen             : array_3x8 := (others => (others => '0'));
   signal tone_gen_op          : std_logic_vector(2 downto 0);
 begin
@@ -162,6 +166,7 @@ begin
     if (ENA = '1') then
       if (I_RESET_L = '0') then -- don't know if reset does reset the sound
         r_snd <= (others => (others => '0'));
+        r_snd(0) <= x"47"; -- D71 As per Chuck Thomka 'The Sound Synthesizer"																			 
       else
         for i in 0 to 7 loop
           if snd_ld(i) then r_snd(i) <= I_MXD; end if;
@@ -275,35 +280,57 @@ begin
   -- x16        0  2  5  8  ms ^  | keypad
   -- x17        ce 1  4  7  mr c  | keypad (left most col)
 
-  -- write
-  -- x10 master osc
-  -- x11 tone a freq
-  -- x12 tone b freq
-  -- x13 tone c freq
-  -- x14 vibrato (7..2 value, 1..0 freq)
-  -- x15 noise control, tone c volume
-  --       bit 5 high to enable noise into mix
-  --       bit 4 high for noise mod, low for vibrato
-  --       bit 3..0 tone c vol
-  -- x16 tone b volume, tone a volume (7..4 b vol, 3..0 a vol)
-  -- x17 noise volume (vol 7..4), 7..0 for master osc modulation
+-- Better Bally Book - Reggs
+               -- 7    6    5    4    3    2    1    0
+-- $10  TONMO   [ f ][ f ][ f ][ f ][ f ][ f ][ f ][ f ]   f = Master oscillator frequency
+-- $11  TONEA   [ f ][ f ][ f ][ f ][ f ][ f ][ f ][ f ]   f = Tone A frequency
+-- $12  TONEB   [ f ][ f ][ f ][ f ][ f ][ f ][ f ][ f ]   f = Tone B frequency
+-- $13  TONEC   [ f ][ f ][ f ][ f ][ f ][ f ][ f ][ f ]   f = Tone C frequency
+-- $14  VIBRA   [ s ][ s ][ v ][ v ][ v ][ v ][ v ][ v ]   s = Vibrato speed (1482), v = Vibrato depth (1488)
+-- $15  VOLC    [ u ][ u ][ n ][ t ][ c ][ c ][ c ][ c ]   u = unused, n = noise switch, t = Tremolo (1476 Mutiplexor Reg) Vibrato(0) Noise(1), c = Tone C
+-- $16  VOLAB   [ b ][ b ][ b ][ b ][ a ][ a ][ a ][ a ]   b = Tone B volume, a = Tone A volume
+-- $17  VOLN    [ N ][ N ][ N ][ N ][ n ][ n ][ n ][ n ]   N = Noise volume register, 4 MSB's noise Vol \ All 8 Bits Tremolo preset
 
-  p_noise_gen            : process
-    variable poly17_zero : std_logic;
+
+ -- 2+4, 6 bit up counter as per patent and Mame = 7 bit counter = x3F or max 63
+ six_bit_up_counter : process
+	begin
+    wait until rising_edge(CLK);
+		if (ENA = '1') then
+			if (I_CPU_ENA = '1') then
+				counter_up <= counter_up + '1';
+			end if;
+		end if;
+
+		if counter_up = "111111" then 
+			counter <= '1';
+			else 
+			counter <= '0'; 
+		end if;
+	end process;
+
+
+-- The noise generator comprises a number generator and is indicated generally at 1496. The number generator comprises a 15-bit shift register 1558 and an exclusive-OR gate indicated at 1560. The inputs of the NOR gates 1494 are connected to the outputs of the 8 most significant bits of the shift register 1558. The output of the two most significant bits are connected to the inputs of the exclusive-OR gate 1560 whose output is connected to the input of the least significant bit of the shift register 1558. The output of the 8 most significant bits of the shift register 1558 is a binary number that constantly changes with each clock signal to the shift register 1558. 
+
+  p_noise_gen            : process -- 15 bit shift register (1558)
+    variable poly15_zero : std_logic;
   begin
-    -- most probably not correct polynomial
     wait until rising_edge(CLK);
     if (ENA = '1') then
-      if (I_CPU_ENA = '1') then
-        poly17_zero := '0';
-        if (poly17 = "00000000000000000") then poly17_zero := '1'; end if;
-        poly17 <= poly17(15 downto 0) & (poly17(16) xor poly17(2) xor poly17_zero);
-      end if;
+		if (I_CPU_ENA = '1') then
+			poly15_zero := '0';
+			if counter = '1' then 
+				poly15_zero := not (poly15(14) xor poly15(13));
+				poly15 <= (poly15(13 downto 0) & poly15_zero); 
+			end if;
+		end if;
     end if;
   end process;
-  noise_gen <= poly17(7 downto 0);
+  noise_gen <= poly15(14 downto 7); -- Noise Generator (1496) Connected to 8 MSB's of shift register 1558
 
-  p_vibrato_osc          : process
+-- The speed of modulation is set by the Vibrato Speed Register (upper 2 bits of output port $14): 00 for fastest and 11 for slowest. The inputs of the transistor switches 1554 are connected to the 4 most significant bits 1556a-d of a counter comprising 13 bits 1556a-m. The output of the 2 bits 1482 (7 & 6) are connected to the inputs of the Vibrato Low Frequency Oscilator (1484)
+
+   p_vibrato_osc          : process -- Vibrato Low Frequency Oscilator (1484)
   begin
     wait until rising_edge(CLK);
     if (ENA = '1') then
@@ -313,53 +340,52 @@ begin
       -- 01 toggle output every  37   mS
       -- 10 toggle output every  74   mS
       -- 11 toggle output every 148   mS
-
-      -- bit 15 every 32768 clocks
       if (I_CPU_ENA = '1') then
-        vibrato_cnt <= vibrato_cnt + "1";
+        vibrato_cnt <= vibrato_cnt + "1"; -- Astrocade 13-bit Vibrato Clock
         vibrato_ena <= '0';
-        case r_snd(4)(1 downto 0) is
-          when "00" => vibrato_ena <= vibrato_cnt(15);
-          when "01" => vibrato_ena <= vibrato_cnt(16);
-          when "10" => vibrato_ena <= vibrato_cnt(17);
-          when "11" => vibrato_ena <= vibrato_cnt(18);
+
+-- x14 Frequency (Bits 7 & 6 (1482) \ 5..0 Vibrato Depth (1488) 
+        case r_snd(4)(7 downto 6) is -- Vibrato Frequency Reg (2 bit 1482)
+          when "00" => vibrato_ena <= vibrato_cnt(9); 
+          when "01" => vibrato_ena <= vibrato_cnt(10);
+          when "10" => vibrato_ena <= vibrato_cnt(11);
+          when "11" => vibrato_ena <= vibrato_cnt(12);
           when others => null;
         end case;
       end if;
     end if;
   end process;
 
-  p_master_freq          : process(vibrato_ena, r_snd, noise_gen)
-    variable mux : std_logic_vector(7 downto 0);
-  begin
-    mux := (others => '0'); -- default
-    if (r_snd(5)(4) = '1') then -- use noise
-      mux := noise_gen and r_snd(7);
-    else
-      if (vibrato_ena = '1') then
-        mux := r_snd(4)(7 downto 2) & "00";
-      else
-        mux := (others => '0');
-      end if;
-    end if;
-    -- add modulation to master osc freq
-    master_freq <= r_snd(0) + mux;
-    -- Arcadian mag claims that the counter is preset to the modulation value
-    -- when the counter hits the master osc reg value.
-    -- The patent / system descriptions describes an adder ....
-  end process;
 
-  p_master_osc           : process
+
+ p_master_osc_freq          : process(vibrato_ena, r_snd, noise_gen, master_freq)
+    variable mux : std_logic_vector(7 downto 0); 	
+  begin
+	master_freq <= (others => '0'); 
+	mux := (others => '0'); -- default, Mux (1474)
+	if (r_snd(5)(4) = '1') then -- use noise \\ Bit 4 is the Multiplexer Register (1476) \\ (0=vibrato, 1=noise)
+		mux (7 downto 0) := noise_gen and r_snd(7); -- All 8 bits = Tremolo preset. (Also 4 MSB's = Noise Volume)
+		master_freq <=  mux;  
+	else 
+		if (vibrato_ena = '1') then -- From Vibrato Frequency Reg (1482)
+			mux := "00" & r_snd(4)(5 downto 0); -- Ground upper 2 bits and send Vibrato Depth, 5 LSB's (1488)
+			master_freq <= mux; -- Ensure Master Oscillator will not count to r_snd(0)
+		end if;
+	end if;
+ end process;
+
+
+ p_master_osc           : process
   begin
     wait until rising_edge(CLK);
     if (ENA = '1') then
       if (I_CPU_ENA = '1') then -- 1.789 Mhz base clock
         master_ena <= '0';
-        if (master_cnt = "00000000") then
+        if (master_cnt = r_snd(0)) then -- Preset Master Oscillator Frequency (TONMO)
           master_cnt <= master_freq;
           master_ena <= '1';
         else
-          master_cnt <= master_cnt - "1";
+          master_cnt <= master_cnt + "1";
         end if;
       end if;
     end if;
@@ -385,7 +411,9 @@ begin
     end if;
   end process;
 
-  p_op_mixer             : process
+-- Patent -- The output of the most significant bit of the shift register 1558 of the noise generator 1496 (poly15(14)) is connected to the input of a NOR gate 1598 whose output is connected by an inverter 1600 to a PLA 1602. The other input of the NOR gate 1598 is connected to the noise modulation register 1536 which is the most significant bit of the output register having address 15H and register select line 1412. The PLA 1602 has inputs connected to the output of the 4 most significant bits of the noise volume register [17H] and the output of the PLA 1602 is also connected to the resistor network 1586. The set of "AND" gates 1530 comprise the plurality of pull-down transistors 1604 of the PLA 1602 with the digital-analog converter 1538 comprising the remainder of the PLA 1602 and resistor network 1586 in a manner similar to the tone generators. The resistor network 1586 has a common summing point 1540 which is connected to the output line 1588 which carries the analog signal AUDIO. In this manner, the AUDIO signal is the sum of the tones A, B and C, generated by the tone generators A, B and C (at their respective volumes), and the noise generator (at its respective volume). 
+
+  p_op_mixer_l           : process
     variable vol : array_4x4;
     variable sum01 : std_logic_vector(4 downto 0);
     variable sum23 : std_logic_vector(4 downto 0);
@@ -401,14 +429,19 @@ begin
 
         if (tone_gen_op(0) = '1') then vol(0) := r_snd(6)(3 downto 0); end if; -- A
         if (tone_gen_op(1) = '1') then vol(1) := r_snd(6)(7 downto 4); end if; -- B
-        if (tone_gen_op(2) = '1') then vol(2) := r_snd(5)(3 downto 0); end if; -- C
-        if (r_snd(5)(5) = '1') then -- noise enable
-          if (noise_gen(0) = '1') then vol(3) := r_snd(5)(7 downto 4); end if; -- noise
-        end if;
+        if (tone_gen_op(2) = '1') then vol(2) := r_snd(5)(3 downto 0); end if; -- C 
 
+-- A 4th tone generator comprises a set of gates indicated at 1530 which functions as 4 AND gates which each have an input operatively connected to a line 1532 which carries a bit from the noise generator 1496 (FIG. 71B). The output of this bit of the noise generator 1496 is a square wave having a constantly varying frequency. The input 1532 is ANDed with 4 volume bits on lines 1534 from the noise volume register 1492 (FIG. 71B). The set of AND gates 1530 operate the same way as the AND gates for the tones A-C, except that a noise modulation register 1536 (having address 15H which activates register select line 1412) must contain a logical 1 for the outputs of the AND gate 1530 to oscillate.
+
+        if (r_snd(5)(5) = '1') then -- Set noise volume if enabled
+			vol(3) := (noise_gen(7 downto 7) and r_snd(7)(7 downto 4)); -- 4 MSB's volume bits And'd with Poly15 MSB 
+		else 
+			vol(3) := (others => '0');  
+        end if;
+	
         sum01 := ('0' & vol(0)) + ('0' & vol(1));
         sum23 := ('0' & vol(2)) + ('0' & vol(3));
-        sum := ('0' & sum01) + ('0' & sum23);
+        sum   := ('0' & sum01)  + ('0' & sum23);
 
         if (I_RESET_L = '0') then
           O_AUDIO <= "00000000";
@@ -416,8 +449,8 @@ begin
           O_AUDIO <= (sum & "00");
         end if;
       end if;
-    end if;
+   end if;
   end process;
-
+ 
 end architecture RTL;
 
